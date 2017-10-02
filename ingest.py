21fae08fe675f6c88ade66996b8f28ce82ea547c
Chris Turner <cat@199tech.net>
2017-10-02 10:59:33 -0500

    ingest.py: cleanup scratch code

--- ingest.py
+++ ingest.py
@@ -1,33 +1,11 @@
 #! /usr/bin/env python
 
-'''
-import unittest2 as unittest
-import numpy as np
-import json
-# from datetime import datetime
-import time
-import os
-import sys
-sys.path.insert(0, '/Users/chris/Workspace/nwb/api-python')
-sys.path.insert(0, '/Users/chris/Workspace/datajoint-python')
-os.chdir('/Users/chris/Workspace/nwb/ALM-1')
-dj.config['database.host'] = 'localhost'
-dj.config['database.user'] = 'chris'
-dj.config['database.password'] = ''
-dj.config['display.limit'] = 5
-dj.config['safemode']= False
-dj.config['ingest.database'] = 'tutorial_alm1_ingest'
-dj.config['production.database'] = 'catalog_alm1_dimitri'
-'''
 import os
 
 import re
 import code
 from decimal import Decimal
-from datetime import datetime
-from contextlib import contextmanager
-
-import yaml
+import datetime  # TODO: proper time string ingest
 
 import datajoint as dj
 
@@ -38,15 +16,18 @@ import h5py
 
 from pymysql.err import IntegrityError
 
+import yaml
 
-[re, code, contextmanager, yaml, dj, nwb_file, nwb_utils, h5py]
+{'unused': [code, datetime, nwb_utils, yaml]}
 # 23456789_123456789_123456789_123456789_123456789_123456789_123456789_12345678
 
-
-def bogus():
-    ''' bogus function to make my navigation shortcuts work '''
-    pass
-
+dj.config['database.host'] = 'localhost'
+dj.config['database.user'] = 'chris'
+dj.config['database.password'] = ''
+dj.config['display.limit'] = 5
+dj.config['safemode'] = False
+dj.config['ingest.database'] = 'tutorial_alm1_ingest'
+dj.config['production.database'] = 'catalog_alm1_dimitri'
 
 schema = dj.schema(dj.config['ingest.database'], locals())
 schema.drop(force=True)
@@ -398,6 +379,13 @@ class CellType(dj.Lookup):
 
 @schema
 class SpikeSorting(dj.Computed):
+    '''
+    SpikeSorting
+    XXX: how to query waveform by trial?
+    e.g.:
+    ['UnitTimes'][unit]['times'][event (797)] -> timestamp
+    ['UnitTimes'][unit]['trial_ids'][event (797)] -> trial
+    '''
 
     definition = """
     -> Ephys
@@ -615,124 +603,4 @@ if __name__ == '__main__':
     Ephys().populate()
     SpikeSorting().populate()
     Acquisition().populate()
-    # code.interact(banner="alm-1 datajoint repl", local=locals())
-
-
-'''
-# misc repl stuffs:
-fn = InputFiles().fetch()[0][0]
-fh = h5py.File(fh, 'r')
-g_gen = fh['general']
-# misc hdf5 stuffs:
-exp[exp.shape].decode()
-'''
-
-'''
-SpikeSorting
-XXX: how to query waveform by trial?
-e.g.:
-['UnitTimes'][unit]['times'][event (797)] -> timestamp
-['UnitTimes'][unit]['trial_ids'][event (797)] -> trial
-'''
-
-
-#
-# _make_tuple_wip
-#
-def _make_tuple_wip(self, key):
-
-    print('yoyo')
-    key['nwb_file'] = (Session() & key).fetch1()['nwb_file']
-    f = h5py.File(key['nwb_file'], 'r')
-
-    g_acq = f['acquisition']
-    self.insert1(key, ignore_extra_fields=True)
-
-    # Acquisition.LickTrace
-    key['lick_trace'] = g_acq['timeseries']['lick_trace']['data']
-    key['timestamps'] = g_acq['timeseries']['lick_trace']['timestamps']
-
-    # FIXME: ok? 21M rows.. hmm.
-    # self.LickTrace().insert1(key, ignore_extra_fields=True)
-
-    # Acquisition.Trial
-    '''
-    alternately:
-    load TrialTypes, UnitInTrial, StimulusPresentation
-    in single top-level trial iteration
-    '''
-    g_epochs = f['epochs']
-    g_analysis = f['analysis']
-    g_pres = f['stimulus']['presentation']
-
-    for tkey in [k for k in g_epochs if 'trial_' in k]:
-
-        # Acquisition.Trial
-        tno = int(tkey.split('_')[1])  # TODO: dup var? (vs string) - cleanup?
-        key['trial'] = tkey.split('_')[1]
-        key['start_time'] = g_epochs[tkey]['start_time'][()]
-        key['stop_time'] = g_epochs[tkey]['stop_time'][()]
-        self.Trial().insert1(key, ignore_extra_fields=True)
-
-        # Acquisition.UnitInTrial
-        d_units = g_epochs[tkey]['units_present']
-        if d_units[(0,)].decode() != 'NA':  # XXX: assuming convention
-            for u in d_units:
-                key['unit'] = u.decode().split('_')[1]
-                self.UnitInTrial().insert1(key, ignore_extra_fields=True)
-
-        # Acquisition.TrialTypes
-        # XXX: ignoring trial_start_times yet doesn't match epoch start
-        # trial_start_times *seems* like unrounded epoch[n]['start_time']?
-
-        ttmat = g_analysis['trial_type_mat']
-        for i in range(len(ttmat)):  # 8 trial types
-            if ttmat[i][(tno - 1)]:  # off-by-1;
-                key['trial_type'] = g_analysis['trial_type_string'][i].decode()
-                self.TrialTypes().insert1(key, ignore_extra_fields=True)
-
-        # Acquisition.StimulusPresentation
-        cuestamps = g_pres['auditory_cue']['timestamps']
-        cuedat = g_pres['auditory_cue']['data']
-
-        ipoletamps = g_pres['pole_in']['timestamps']
-        opolestamps = g_pres['pole_out']['timestamps']
-
-        tidx = tno - 1
-        key['auditory_timestamp'] = Decimal(float(cuestamps[tidx]))
-        key['auditory_cue'] = cuedat[tidx]
-        key['pole_in_timestamp'] = ipoletamps[tidx]
-        key['pole_out_timestamp'] = opolestamps[tidx]
-        try:
-            self.StimulusPresentation().insert1(key, ignore_extra_fields=True)
-        except IntegrityError:  # TODO: handle NaN in timestamps
-            print('.StimulusPresentation error: session', key['session'],
-                  'trial:', key['trial'])
-
-    f.close()
-
-
-def _make_tuple_test(cls, func=None):
-    if func is None:
-        if '_orig_make_tuples' in cls.__dict__:
-            print('reset')
-            cls._make_tuples = cls._orig_make_tuples
-
-    else:
-        if '_orig_make_tuples' not in cls.__dict__:
-            print('backup')
-            cls._orig_make_tuples = cls._make_tuples
-
-        print('replace')
-        cls._make_tuples = func
-
-    cls().delete()
-    cls().populate()
-    return cls()
-
-
-def _make_tuple_loop():
-    # doh: need 'force=true'alike for auto looping;
-    # plus auto loop prevents redefining function in repl...
-    pass
-
+    # code.interact(banner="alm-1 datajoint ingest schema", local=locals())
